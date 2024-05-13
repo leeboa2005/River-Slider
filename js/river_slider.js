d80dd8d4a621441d0f88d37faf2db10b6763dd57
leeboa2005 <leeboa2005@naver.com>
2024-05-13 11:07:10 +0900

    json 파일 추가 및 js코드 수정

--- js/river_slider.js
+++ js/river_slider.js
@@ -1,59 +1,89 @@
-const riverSlides = document.querySelector(".river_slides");
-const riverPlayBtn = document.querySelector(".river_stop_btn");
-const riverStopBtn = document.querySelector(".river_play_btn");
+"use script";
+// 각각의 슬라이드 data를 불러옴
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
 
-// river_slides를 한번 더 복사하는 함수
+const riverMovieWrap = document.querySelector(".river_slides_wrap");
+const riverSlides = document.querySelector(".river_slides");
+const riverStopBtn = document.querySelector(".river_stop_btn");
+const riverPlayBtn = document.querySelector(".river_play_btn");
+
+// 각각의 슬라이드들이 화면에 출력됨
+function displayItems(items) {
+  riverSlides.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+// 각각의 슬라이드에 data값 적용 후 슬라이드 생성
+function createHTMLString(item) {
+  return `
+  <li>
+  <a href="${item.link}" target="_blank" aria-label="${item.title}">
+    <img src="${item.img}" alt="${item.title}">
+    <h2 class="river_slides-content">
+      <span class="river_slides_cta">상세정보</span>
+    </h2>
+  </a>
+</li>
+    `;
+}
+
+// river_slides를 복사 기능
 function clone() {
   const clone = riverSlides.cloneNode(true);
-  const riverMovieWrap = document.querySelector(".river_slides_wrap");
   riverMovieWrap.appendChild(clone);
-
-  riverSlides.offsetWidth + "px";
-
   riverSlides.classList.add("original");
   clone.classList.add("clone");
 }
 
-clone();
+// 슬라이드들이 화면에 출력된후 river_slides를 복사함
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    clone();
+  })
+  .catch(() => console.log("Failed..."));
 
 // 애니메이션 재생 시키는 함수
-function animationRunning() {
+function animationRunning(run) {
   const riverOriginal = document.querySelector(".river_slides.original");
   const riverClone = document.querySelector(".river_slides.clone");
-  riverOriginal.style.WebkitAnimationPlayState = "running";
-  riverClone.style.WebkitAnimationPlayState = "running";
+  riverOriginal.style.WebkitAnimationPlayState = `${run}`;
+  riverClone.style.WebkitAnimationPlayState = `${run}`;
 }
 
 // 애니메이션 정지 시키는 함수
-function animationPaused() {
+function animationPaused(stp) {
   const riverOriginal = document.querySelector(".river_slides.original");
   const riverClone = document.querySelector(".river_slides.clone");
-  riverOriginal.style.WebkitAnimationPlayState = "paused";
-  riverClone.style.WebkitAnimationPlayState = "paused";
+  riverOriginal.style.WebkitAnimationPlayState = `${stp}`;
+  riverClone.style.WebkitAnimationPlayState = `${stp}`;
 }
 
 //클릭했을때 재생, 정지 이벤트
-riverStopBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  animationRunning();
-  riverStopBtn.classList.remove("active");
-  riverPlayBtn.classList.add("active");
-});
-
 riverPlayBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  animationPaused();
+  animationRunning("running");
   riverPlayBtn.classList.remove("active");
   riverStopBtn.classList.add("active");
 });
 
-//마우스 호버했을때 재생, 정지 이벤트
-riverSlides.addEventListener("mouseenter", () => {
-  animationPaused();
+riverStopBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  animationPaused("paused");
+  riverStopBtn.classList.remove("active");
+  riverPlayBtn.classList.add("active");
 });
 
-riverSlides.addEventListener("mouseleave", () => {
-  if (riverPlayBtn.classList.contains("active")) {
-    animationRunning();
+//마우스 호버했을때 재생, 정지 이벤트
+riverMovieWrap.addEventListener("mouseenter", () => {
+  animationPaused("paused");
+});
+
+riverMovieWrap.addEventListener("mouseleave", () => {
+  if (riverStopBtn.classList.contains("active")) {
+    animationRunning("running");
   }
 });
